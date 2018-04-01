1d9d808d4662044d514f37d313617df8997da2fb
misterbingo <aandthea@gmail.com>
2018-04-01 15:34:42 +0100

    0.14
    Fixed color codes on all letters. Added temporary screen completely. Started Tetris gameflow with bag generation and piece assignment.

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-import serial,evdev,time
+import serial,evdev,time,os,sys
+
+def setupScreen():
+    global temparray
+    temparray=[]
+    for i in range(0,19):
+        temparray.append("XXXXXXXXX\n")                      
+    for i in range(0,19):
+        sys.stdout.write(temparray[i])
 
 def updatePixel(x,y,colorcode):
-    print("Not implemented")
+    blank=os.system("clear")
+    if colorcode==1:
+        temparray[y]=(temparray[y][0:x]+"X"+temparray[y][x+1:10])
+    else:
+        temparray[y]=(temparray[y][0:x]+"#"+temparray[y][x+1:10])
+    for i in reversed(temparray):
+        sys.stdout.write(i)
+    print("\n")
     #running arduino code using the pi (not sure about implementation yet)
     #COLOR CODES:
     #1 Empty
@@ -26,15 +41,15 @@ class sprite:
             updatePixel(pixel[0]+self.location[0],pixel[1]+self.location[1],pixel[2])
     def eraseSprite(self):#updates all the pixels at the co ordinates relative to the position to 0 making them black
         for pixel in self.pixels:
-            updatePixel(pixel[0]+self.location[0],pixel[1]+self.location[1],0)
+            updatePixel(pixel[0]+self.location[0],pixel[1]+self.location[1],1)
 
 class piece(sprite):
     def __init__(self,name,shape,location,rot):
         self.name=name
         self.shape=shape
         self.location=location
-        self.rotation="0"
-        self.updateShape(shape,"0")
+        self.pixels=[]
+        self.updateShape(shape,rot)
     def updateShape(self,shape,rot):
         #This long if series sets the pixels of the sprite according to the shape with
         #a lil diagram next to it # is a block and X is the centre
@@ -178,258 +193,259 @@ class piece(sprite):
         #Translates piece
     def rotatePiece(self,grid,dir):
         ###CYCLE 2
+        print()
 class character(piece):
     #the letter class is like the shape but instead of strings defining the shape, they are defined by integers from 0 to 25 so that they can be scrolled through later
     # 0-A 1-B 2-C 3-D 4-E 5-F 6-G 7-H 8-I 9-J 10-K 11-L 12-M 13-N 14-O 15-P 16-Q 17-R 18-S 19-T 20-U 21-V 22-W 23-X 24-Y 25-Z
     #borrowed from https://robey.lag.net/2010/01/23/tiny-monospace-font.html
-    def updateShape(self,shape,0):
+    def updateShape(self,shape,rot):
         if shape==0:
-            self.pixels=[[0,0,1],[2,0,1],[0,1,1],[2,1,1],[0,2,1],[1,2,1],[2,2,1],[0,3,1],[2,3,1],[1,4,1]]
+            self.pixels=[[0,0,2],[2,0,2],[0,1,2],[2,1,2],[0,2,2],[1,2,2],[2,2,2],[0,3,2],[2,3,2],[1,4,2]]
              #
             # #
             ###
             # #
         #   X #
-        if shape==1:
-            self.pixels=[[0,0,1],[1,0,1],[0,1,1],[2,1,1],[0,2,1],[1,2,1],[0,3,1],[2,3,1],[0,4,1],[1,4,1]]
+        elif shape==1:
+            self.pixels=[[0,0,2],[1,0,2],[0,1,2],[2,1,2],[0,2,2],[1,2,2],[0,3,2],[2,3,2],[0,4,2],[1,4,2]]
             ##
             # #
             ##
             # #
         #   X#
-        if shape==2:
-            self.pixels=[[1,0,1],[2,0,1],[0,1,1],[0,2,1],[0,3,1],[1,4,1],[2,4,1]]
+        elif shape==2:
+            self.pixels=[[1,0,2],[2,0,2],[0,1,2],[0,2,2],[0,3,2],[1,4,2],[2,4,2]]
              ##
             #
             #
             #
         #   X##
-        if shape==3:
-            self.pixels=[[0,0,1],[1,0,1],[0,1,1],[2,1,1],[0,2,1],[2,2,1],[0,3,1],[2,3,1],[0,4,1],[1,4,1]]
+        elif shape==3:
+            self.pixels=[[0,0,2],[1,0,2],[0,1,2],[2,1,2],[0,2,2],[2,2,2],[0,3,2],[2,3,2],[0,4,2],[1,4,2]]
             ##
             # #
             # #
             # #
         #   X#
-        if shape==4:
-            self.pixels=[[0,0,1],[1,0,1],[2,0,1],[0,1,1],[0,2,1],[1,2,1],[2,2,1],[0,3,1],[0,4,1],[1,4,1],[2,4,1]]
+        elif shape==4:
+            self.pixels=[[0,0,2],[1,0,2],[2,0,2],[0,1,2],[0,2,2],[1,2,2],[2,2,2],[0,3,2],[0,4,2],[1,4,2],[2,4,2]]
             ###
             #
             ###
             #
         #   X##
-        if shape==5:
-            self.pixels=[[0,0,1],[0,1,1],[0,2,1],[1,2,1],[2,2,1],[0,3,1],[0,4,1],[1,4,1],[2,4,1]]
+        elif shape==5:
+            self.pixels=[[0,0,2],[0,1,2],[0,2,2],[1,2,2],[2,2,2],[0,3,2],[0,4,2],[1,4,2],[2,4,2]]
             ###
             #
             ###
             #
         #   X
-        if shape==6:
-            self.pixels=[[1,0,1],[2,0,1],[0,1,1],[2,1,1],[0,2,1],[1,2,1],[2,2,1],[0,3,1],[1,4,1],[2,4,1]]
+        elif shape==6:
+            self.pixels=[[1,0,2],[2,0,2],[0,1,2],[2,1,2],[0,2,2],[1,2,2],[2,2,2],[0,3,2],[1,4,2],[2,4,2]]
              ##
             #
             ###
             # #
         #   X##
-        if shape==7:
-            self.pixels=[[0,0,1],[2,0,1],[0,1,1],[2,1,1],[0,2,1],[1,2,1],[2,2,1],[0,3,1],[2,3,1],[0,4,1],[2,4,1]]
+        elif shape==7:
+            self.pixels=[[0,0,2],[2,0,2],[0,1,2],[2,1,2],[0,2,2],[1,2,2],[2,2,2],[0,3,2],[2,3,2],[0,4,2],[2,4,2]]
             # #
             # #
             ###
             # #
         #   X #
-        if shape==8:
-            self.pixels=[[0,0,1],[1,0,1],[2,0,1],[1,1,1],[1,2,1],[1,3,1],[0,4,1],[1,4,1],[2,4,1]]
+        elif shape==8:
+            self.pixels=[[0,0,2],[1,0,2],[2,0,2],[1,1,2],[1,2,2],[1,3,2],[0,4,2],[1,4,2],[2,4,2]]
             ###
              #
              #
              #
         #   X##
-        if shape==9:
-            self.pixels=[[1,0,1],[0,1,1],[2,1,1],[2,2,1],[2,3,1,],[2,4,1]]
+        elif shape==9:
+            self.pixels=[[1,0,2],[0,1,2],[2,1,2],[2,2,2],[2,3,1,],[2,4,2]]
               #
               #
               #
             # #
         #   X#
-        if shape==10:
-            self.pixels=[[0,0,1],[2,0,1],[0,1,1],[2,1,1],[0,2,1],[1,2,1],[0,3,1],[2,3,1],[0,4,1],[2,4,1]]
+        elif shape==10:
+            self.pixels=[[0,0,2],[2,0,2],[0,1,2],[2,1,2],[0,2,2],[1,2,2],[0,3,2],[2,3,2],[0,4,2],[2,4,2]]
             # #
             # #
             ##
             # #
         #   X #
-        if shape==11:
-            self.pixels=[[0,0,1],[1,0,1],[2,0,1],[0,1,1],[0,2,1],[0,3,1],[0,4,1]]
+        elif shape==11:
+            self.pixels=[[0,0,2],[1,0,2],[2,0,2],[0,1,2],[0,2,2],[0,3,2],[0,4,2]]
             #
             #
             #
             #
         #   X##
-        if shape==12:
-            self.pixels=[[0,0,1],[2,0,1],[0,1,1],[2,1,1],[0,2,1],[1,2,1],[2,2,1],[0,3,1],[1,3,1],[2,3,1],[0,4,1],[2,4,1]]
+        elif shape==12:
+            self.pixels=[[0,0,2],[2,0,2],[0,1,2],[2,1,2],[0,2,2],[1,2,2],[2,2,2],[0,3,2],[1,3,2],[2,3,2],[0,4,2],[2,4,2]]
             # #
             ###
             ###
             # #
         #   X #
-        if shape==13:
-            self.pixels=[[0,0,1],[2,0,1],[0,1,1],[1,1,1],[2,1,1],[0,2,1],[1,2,1],[2,2,1],[0,3,1],[1,3,1],[2,3,1],[0,4,1],[2,4,1]]
+        elif shape==13:
+            self.pixels=[[0,0,2],[2,0,2],[0,1,2],[1,1,2],[2,1,2],[0,2,2],[1,2,2],[2,2,2],[0,3,2],[1,3,2],[2,3,2],[0,4,2],[2,4,2]]
             # #
             ###
             ###
             ###
         #   X #
-        if shape==14:
-            self.pixels=[[1,0,1],[0,1,1],[2,1,1],[0,2,1],[2,2,1],[0,3,1],[2,3,1],[1,4,1]]
+        elif shape==14:
+            self.pixels=[[1,0,2],[0,1,2],[2,1,2],[0,2,2],[2,2,2],[0,3,2],[2,3,2],[1,4,2]]
              #
             # #
             # #
             # #
         #   X#
-        if shape==15:
-            self.pixels=[[0,0,1],[0,1,1],[0,2,1],[1,2,1],[0,3,1],[2,3,1],[0,4,1],[1,4,1]]
+        elif shape==15:
+            self.pixels=[[0,0,2],[0,1,2],[0,2,2],[1,2,2],[0,3,2],[2,3,2],[0,4,2],[1,4,2]]
             ##
             # #
             ##
             #
         #   X
-        if shape==16:
-            self.pixels=[[1,0,1],[2,0,1],[0,1,1],[1,1,1],[2,1,1],[0,2,1],[2,2,1],[0,3,1],[2,3,1],[1,4,1]]
+        elif shape==16:
+            self.pixels=[[1,0,2],[2,0,2],[0,1,2],[1,1,2],[2,1,2],[0,2,2],[2,2,2],[0,3,2],[2,3,2],[1,4,2]]
              #
             # #
             # #
             ###
         #   X##
-        if shape==17:
-            self.pixels=[[0,0,1],[2,0,1],[0,1,1],[1,1,1],[0,2,1],[1,2,1],[2,2,1],[0,3,1],[2,3,1],[0,4,1],[1,4,1]]
+        elif shape==17:
+            self.pixels=[[0,0,2],[2,0,2],[0,1,2],[1,1,2],[0,2,2],[1,2,2],[2,2,2],[0,3,2],[2,3,2],[0,4,2],[1,4,2]]
             ##
             # #
             ###
             ##
         #   X #
-        if shape==18:
-            self.pixels=[[0,0,1],[1,0,1],[2,1,1],[1,2,1],[0,3,1],[1,4,1],[2,4,1]]
+        elif shape==18:
+            self.pixels=[[0,0,2],[1,0,2],[2,1,2],[1,2,2],[0,3,2],[1,4,2],[2,4,2]]
              ##
             #
              #
               #
         #   X#
-        if shape==19:
-            self.pixels=[[1,0,1],[1,1,1],[1,2,1],[1,3,1],[0,4,1],[1,4,1],[2,4,1]]
+        elif shape==19:
+            self.pixels=[[1,0,2],[1,1,2],[1,2,2],[1,3,2],[0,4,2],[1,4,2],[2,4,2]]
             ###
              #
              #
              #
         #   X#
-        if shape==20:
-            self.pixels=[[1,0,1],[2,0,1],[0,1,1],[2,1,1],[0,2,1],[2,2,1],[0,3,1],[2,3,1],[0,4,1],[2,4,1]]
+        elif shape==20:
+            self.pixels=[[1,0,2],[2,0,2],[0,1,2],[2,1,2],[0,2,2],[2,2,2],[0,3,2],[2,3,2],[0,4,2],[2,4,2]]
             # #
             # #
             # #
             # #
              ##
-        if shape==21:
-            self.pixels=[[1,0,1],[1,1,1],[0,2,1],[2,2,1],[0,3,1],[2,3,1],[0,4,1],[2,4,1]]
+        elif shape==21:
+            self.pixels=[[1,0,2],[1,1,2],[0,2,2],[2,2,2],[0,3,2],[2,3,2],[0,4,2],[2,4,2]]
             # #
             # #
             # #
              #
         #   X#
-        if shape==22:
-            self.pixels=[[0,0,1],[2,0,1],[0,1,1],[1,1,1],[2,1,1],[0,2,1],[1,2,1],[2,2,1],[0,3,1],[2,3,1],[0,4,1],[2,4,1]]
+        elif shape==22:
+            self.pixels=[[0,0,2],[2,0,2],[0,1,2],[1,1,2],[2,1,2],[0,2,2],[1,2,2],[2,2,2],[0,3,2],[2,3,2],[0,4,2],[2,4,2]]
             # #
             # #
             ###
             ###
         #   X #
-        if shape==23:
-            self.pixels=[[0,0,1],[2,0,1],[0,1,1],[2,1,1],[1,2,1],[0,3,1],[2,3,1],[0,4,1],[2,4,1]]
+        elif shape==23:
+            self.pixels=[[0,0,2],[2,0,2],[0,1,2],[2,1,2],[1,2,2],[0,3,2],[2,3,2],[0,4,2],[2,4,2]]
             # #
             # #
              #
             # #
         #   X #
-        if shape==24:
-            self.pixels=[[1,0,1],[1,1,1],[1,2,1],[0,3,1],[2,3,1],[0,4,1],[2,4,1]]
+        elif shape==24:
+            self.pixels=[[1,0,2],[1,1,2],[1,2,2],[0,3,2],[2,3,2],[0,4,2],[2,4,2]]
             # #
             # #
              #
              #
         #   X#
-        if shape==25:
-            self.pixels=[[0,0,1],[1,0,1],[2,0,1],[0,1,1],[1,2,1],[2,3,1],[0,4,1],[1,4,1],[2,4,1]]
+        elif shape==25:
+            self.pixels=[[0,0,2],[1,0,2],[2,0,2],[0,1,2],[1,2,2],[2,3,2],[0,4,2],[1,4,2],[2,4,2]]
             ###
               #
              #
             #
         #   X##
-        if shape==26:
-            self.pixels=[[0,0,1],[1,0,1],[2,0,1],[0,1,1],[2,1,1],[0,2,1],[2,2,1],[0,3,1],[2,3,1],[0,4,1],[1,4,1],[2,4,1]
+        elif shape==26:
+            self.pixels=[[0,0,2],[1,0,2],[2,0,2],[0,1,2],[2,1,2],[0,2,2],[2,2,2],[0,3,2],[2,3,2],[0,4,2],[1,4,2],[2,4,2]]
             ###
             # #
             # #
             # #
         #   X##
-        if shape==27:
-            self.pixels=[[1,0,1],[1,1,1],[1,2,1],[1,3,1],[1,4,1]]
+        elif shape==27:
+            self.pixels=[[1,0,2],[1,1,2],[1,2,2],[1,3,2],[1,4,2]]
              #
              #
              #
              #
         #   X#
-        if shape==28:
-            self.pixels=[[0,0,1],[1,0,1],[2,0,1],[0,1,1],[0,2,1],[1,2,1][2,2,1],[2,3,1],[0,4,1],[1,4,1],[2,4,1]]
+        elif shape==28:
+            self.pixels=[[0,0,2],[1,0,2],[2,0,2],[0,1,2],[0,2,2],[1,2,2][2,2,2],[2,3,2],[0,4,2],[1,4,2],[2,4,2]]
             ###
               #
             ###
             #
         #   X##
-        if shape==29:
-            self.pixels=[[0,0,1],[1,0,1],[2,0,1],[2,1,1],[1,2,1],[2,2,1],[2,3,1],[0,4,1],[1,4,1],[2,4,1]]
+        elif shape==29:
+            self.pixels=[[0,0,2],[1,0,2],[2,0,2],[2,1,2],[1,2,2],[2,2,2],[2,3,2],[0,4,2],[1,4,2],[2,4,2]]
             ###
               #
              ##
               #
         #   X##
-        if shape==30:
-            self.pixels=[[2,0,1],[2,1,1],[0,2,1],[1,2,1],[2,2,1],[0,3,1],[2,3,1],[0,4,1],[2,4,1]]
+        elif shape==30:
+            self.pixels=[[2,0,2],[2,1,2],[0,2,2],[1,2,2],[2,2,2],[0,3,2],[2,3,2],[0,4,2],[2,4,2]]
             # #
             # #
             ###
               #
         #   X #
-        if shape==31:
-            self.pixels=[[0,0,1],[1,0,1],[2,0,1],[2,1,1],[0,2,1],[1,2,1],[2,2,1],[0,3,1],[0,4,1],[1,4,1],[2,4,1]]
+        elif shape==31:
+            self.pixels=[[0,0,2],[1,0,2],[2,0,2],[2,1,2],[0,2,2],[1,2,2],[2,2,2],[0,3,2],[0,4,2],[1,4,2],[2,4,2]]
             ###
             #
             ###
               #
         #   X##
-        if shape==32:
-            self.pixels=[[0,0,1],[1,0,1],[2,0,1],[0,1,1],[2,1,1],[0,2,1],[2,2,1],[0,3,1],[1,4,1],[2,4,1]]
+        elif shape==32:
+            self.pixels=[[0,0,2],[1,0,2],[2,0,2],[0,1,2],[2,1,2],[0,2,2],[2,2,2],[0,3,2],[1,4,2],[2,4,2]]
              ##
             #
             ###
             # #
         #   X##
-        if shape==33:
-            self.pixels=[[0,0,1],[0,1,1],[1,2,1],[2,3,1],[0,4,1],[1,4,1],[2,4,1]]
+        elif shape==33:
+            self.pixels=[[0,0,2],[0,1,2],[1,2,2],[2,3,2],[0,4,2],[1,4,2],[2,4,2]]
             ###
               #
              #
             #
         #   X
-        if shape=34:
-            self.pixels=[[0,0,1],[1,0,1],[2,0,1],[0,1,1],[2,1,1],[0,2,1],[1,2,1],[2,2,1],[0,3,1],[2,3,1],[0,4,1],[1,4,1],[2,4,1]]
+        elif shape==34:
+            self.pixels=[[0,0,2],[1,0,2],[2,0,2],[0,1,2],[2,1,2],[0,2,2],[1,2,2],[2,2,2],[0,3,2],[2,3,2],[0,4,2],[1,4,2],[2,4,2]]
             ###
             # #
             ###
             # #
         #   X##
-        if shape=35:
-            self.pixels=[[0,0,1],[1,0,1],[2,1,1],[0,2,1],[1,2,1],[2,2,1],[0,3,1],[2,3,1],[0,4,1],[1,4,1],[2,4,1]]
+        elif shape==35:
+            self.pixels=[[0,0,2],[1,0,2],[2,1,2],[0,2,2],[1,2,2],[2,2,2],[0,3,2],[2,3,2],[0,4,2],[1,4,2],[2,4,2]]
             ###
             # #
             ###
@@ -451,7 +467,7 @@ def waitForInput(gamepad,t):
             event=gamepad.read_one()
             if event!=None:
                     if event.type!=0 and event.type!=4:
-                            padin="_"
+                            padin=""
                             if (event.type==1 and event.value!=0L):
                                     if event.code==304:
                                             padin="A"
@@ -479,13 +495,13 @@ def waitForInput(gamepad,t):
                             timeDone=True
                             return(None)
 
-def runMenu:
+def runMenu():
     items=[piece("gameSelect","T",[4,13],0),piece("pointer","I",[4,11],0),character("scoreSelect",18,[4,3],0)]
-    for object in items:
-        object.drawSprite()
+    for item in items:
+        item.drawSprite()
     selected="game"
     confirmed=False
-    while not confirmed:
+    while (not confirmed):
         userin=waitForInput(gamePad,0)
         if (userin=="DOWN" and selected=="game"):
             items[1].translatePiece([0,-10])
@@ -496,21 +512,37 @@ def runMenu:
         if userin=="A":
             confirmed==True
     removeSprites(items)
-
-    if selected="game":
-        ###CYCLE 2
+    if selected=="game":
+        runGame()
     if selected=="scores":
-        ###CYCLE 3
+        runScores()
 
-    for object in items:
-        object.eraseSprite:
-        items.remove(object)
+    removeSprites(items)
 
+def runGame():
+    items=[sprite("grid",[],[0,0])]
+    score=0
+    gameover=False
+    T=0.5
+    userin=""
+    allpieces=[piece("iPiece","I",[6,20],0),piece("jPiece","J",[6,20],0),piece("lPiece","L",[6,20],0),piece("sPiece","S",[6,20],0),piece("oPiece","O",[6,20],0),piece("zPiece","Z",[6,20],0),piece("tPiece","T",[6,20],0)]
+    nextpiece=random.choice(allpieces)
+    bag=[]
+    while(not userin=="START"):
+        userin=waitforInput(gamePad,0)
+    while(not gameover):
+        if bag==None:
+            bag=random.shuffle(allpieces)
+        
+            
+
+def runScores():
+    items=[]
 
 ####################
 ####MAIN PROGRAM####
 ####################
-
- gamePad=evdev.InputDevice('PATH TO DEVICE') #Placeholder path to device /dev/input/eventN
+setupScreen()
+gamePad=evdev.InputDevice("/dev/input/event0") #Placeholder path to device /dev/input/eventN
 while True:
-runMenu()
+    runMenu()
